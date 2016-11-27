44f33ece3afb49705c6a891638dd66e0e277f65b
Nicolas França <nicolasfranca9@gmail.com>
2016-11-27 18:16:07 -0200

    Finished Chapter 8. \o/

--- depot/app/models/product.rb
+++ depot/app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ApplicationRecord
   validates :title, :description, :image_url, presence: true
-  validates :price, numericality: {greater_than_or_equal_to: 0.01}
-  validates :title, uniqueness: true
+  validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+  validates :title, uniqueness: true, length: { minimum: 10 }
   validates :image_url, allow_blank: true, format: {
     with: %r{\.(gif|jpg|png)\Z}i,
     message: 'must be a URL for GIF, JPG, or PNG image.'
--- depot/config/routes.rb
+++ depot/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'store#index', as: 'store_index'
+
   resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
