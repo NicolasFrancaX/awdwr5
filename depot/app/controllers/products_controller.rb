8e053e2bd454742c0c366a0c2581bc68526427c7
Nicolas França <nicolasfranca9@gmail.com>
2016-12-21 17:14:40 -0200

    Chapter 25. \o

--- depot/app/controllers/products_controller.rb
+++ depot/app/controllers/products_controller.rb
@@ -1,6 +1,8 @@
 class ProductsController < ApplicationController
   before_action :set_product, only: [:show, :edit, :update, :destroy]
 
+  attr
+
   # GET /products
   # GET /products.json
   def index
